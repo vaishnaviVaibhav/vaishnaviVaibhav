08ffcc08bbdf47c9225638fc31a2b321a754e7b3
dac <dac@dypm.org>
2023-08-28 13:00:22 +0530

    restWithMysql added

---
+++ Entities/TestContext.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace RestwithMysql.Entities;
+
+public partial class TestContext : DbContext
+{
+    public TestContext()
+    {
+    }
+
+    public TestContext(DbContextOptions<TestContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<User> Users { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseMySQL("server=localhost;port=3306;user=root;password=root123;database=test");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PRIMARY");
+
+            entity.ToTable("user");
+
+            entity.Property(e => e.Id).HasColumnType("int(11)");
+            entity.Property(e => e.EnrollmentDate).HasColumnType("datetime");
+            entity.Property(e => e.FirstName).HasMaxLength(45);
+            entity.Property(e => e.LastName).HasMaxLength(45);
+            entity.Property(e => e.Password).HasMaxLength(45);
+            entity.Property(e => e.Username).HasMaxLength(45);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
